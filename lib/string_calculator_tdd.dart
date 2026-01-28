dfbb7307787d233e99896d2f09b74736fc2901b6
Sourav Sonkar <souravsonkar@192.168.1.3>
2026-01-29 00:54:56 +0530

    refactor: remove special case for single input

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -2,13 +2,11 @@ int add(String numbers) {
   if (numbers.isEmpty) return 0;
 
   final parts = numbers.split(',');
-  if (parts.length == 1) {
-    return int.parse(parts[0]);
-  }
-  
   int sum = 0;
-  for (var i in parts) {
-    sum += int.parse(i);
+
+  for (final part in parts) {
+    sum += int.parse(part);
   }
+
   return sum;
 }

0dd783e2bd9036f06ace93ebdb569d18f1ca9e0c
Sourav Sonkar <souravsonkar@192.168.1.3>
2026-01-29 00:20:18 +0530

    test: add failing test for empty string

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,7 +2,14 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('return 0 when input is empty string', () {
+    // Given
+    final String input = '';
+    
+    // When
+    final result = add(input);
+
+    // Then
+    expect(result, 0);
   });
 }

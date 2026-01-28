ac87ab5b00906630e65e32284493b0bd83eab668
Sourav Sonkar <souravsonkar@192.168.1.3>
2026-01-29 00:26:19 +0530

    test: add test for single number

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -5,11 +5,22 @@ void main() {
   test('return 0 when input is empty string', () {
     // Given
     final String input = '';
-    
+
     // When
     final result = add(input);
 
     // Then
     expect(result, 0);
+  });
+
+  test('return number for single input', () {
+    // Given
+    final String input = '1';
+
+    // When
+    final result = add(input);
+
+    // Then
+    expect(result, 1);
   });
 }

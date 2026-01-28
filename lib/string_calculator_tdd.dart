2f221420778c6f964177dbd474c3f3f6436b2b32
Sourav Sonkar <souravsonkar@192.168.1.3>
2026-01-29 01:44:34 +0530

    feat: added support for custom delimiter

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,7 +1,14 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
+  String delimiter = ',';
+  
+  if(numbers.startsWith('//')){
+    final split = numbers.split('\n');
+    delimiter = split.first.substring(2);
+    numbers = split.last;
+  }
 
-  final parts = numbers.replaceAll('\n', ',').split(',');
+  final parts = numbers.replaceAll('\n', delimiter).split(delimiter);
   int sum = 0;
 
   for (final part in parts) {

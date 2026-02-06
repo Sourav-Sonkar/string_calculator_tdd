45c3af146b78787ccda6a99e07974c7884fb54a7
Sourav Sonkar <souravsonkar@192.168.1.2>
2026-02-06 13:31:14 +0530

    feat: added functionality to multiply numbers when delimiter is *

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,7 +1,37 @@
-int add(String numbers) {
+int operation(String numbers) {
   if (numbers.isEmpty) return 0;
   String delimiter = ',';
 
+  (numbers, delimiter) = extractDelimiter(numbers, delimiter);
+
+  final parts = numbers.replaceAll('\n', delimiter).split(delimiter);
+  int sum = 0;
+  int product = 1;
+  bool shouldMultiply = delimiter == '*';
+
+  final negatives = <int>[];
+  for (final part in parts) {
+    int curr = int.parse(part);
+    if (curr < 0) negatives.add(curr);
+    if (curr <= 1000) {
+      if (shouldMultiply) {
+        product *= int.parse(part);
+      } else {
+        sum += int.parse(part);
+      }
+    }
+  }
+  if (negatives.isNotEmpty) {
+    throw UnimplementedError('Negatives not allowed ie ${negatives.join(',')}');
+  }
+
+  return shouldMultiply ? product : sum;
+}
+
+(String numbers, String delimiter) extractDelimiter(
+  String numbers,
+  String delimiter,
+) {
   if (numbers.startsWith('//')) {
     final split = numbers.split('\n');
     final delimiterPart = split.first.substring(2);
@@ -13,19 +43,5 @@ int add(String numbers) {
     numbers = split.last;
   }
 
-  final parts = numbers.replaceAll('\n', delimiter).split(delimiter);
-  int sum = 0;
-  final negatives = <int>[];
-  for (final part in parts) {
-    int curr = int.parse(part);
-    if (curr < 0) negatives.add(curr);
-    if (curr <= 1000) {
-      sum += int.parse(part);
-    }
-  }
-  if (negatives.isNotEmpty) {
-    throw UnimplementedError('Negatives not allowed ie ${negatives.join(',')}');
-  }
-
-  return sum;
+  return (numbers, delimiter);
 }
